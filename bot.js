374f2fc00a31646a09e78c916a6a221403b3b98e
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2023-05-17 00:01:51 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -115,12 +115,12 @@ client.on("messageCreate", async (message) => {
         const emote_uwu = "<:uwu:1074499520462860369>";
         const emote_xdx = "<:xdx:1074494997996511242> ";
         const emote_flower = "<:peepoflower:1059683310080626688>";
-        
+        const emote_sadge = "<:sadge:1059683257265954887>";
         const regex_uwu = new RegExp("uwu", "gi");
         const regex_xdx = new RegExp("xdx", "gi");
         const regex_o = new RegExp(":o", "gi");
 
-        await message.reply(respuesta.text.replaceAll(regex_xdx, emote_xdx).replaceAll(/xD/g, emote_xdx).replaceAll(regex_uwu, emote_uwu).replaceAll(/<3/g, emote_cora).replaceAll(regex_o, emote_monku).replaceAll(/:3/g, emote_flower));
+        await message.reply(respuesta.text.replaceAll(regex_xdx, emote_xdx).replaceAll(/xD/g, emote_xdx).replaceAll(regex_uwu, emote_uwu).replaceAll(/<3/g, emote_cora).replaceAll(regex_o, emote_monku).replaceAll(/:3/g, emote_flower).replaceAll(":(", emote_sadge));
       }
       catch (error) {
         console.log(error);
